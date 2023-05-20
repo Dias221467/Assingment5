f31ce0a904510f969248e8304a5352140ce1e2d7
Диас <lbrjyz05@mail.ru>
2023-05-20 21:32:47 +0600

    2

--- BST.java
+++ BST.java
@@ -1,2 +1,153 @@
-public class BST {
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+        private int size;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+            this.size = 1;
+        }
+    }
+
+    public class KeyValuePair {
+        private K key;
+        private V val;
+
+        public KeyValuePair(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return val;
+        }
+    }
+
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            return new Node(key, val);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        } else {
+            node.val = val;
+        }
+
+        node.size = 1 + size(node.left) + size(node.right);
+        return node;
+    }
+
+    public V get(K key) {
+        Node node = get(root, key);
+        return node != null ? node.val : null;
+    }
+
+    private Node get(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        } else {
+            return node;
+        }
+    }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = delete(node.left, key);
+        } else if (cmp > 0) {
+            node.right = delete(node.right, key);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            } else if (node.right == null) {
+                return node.left;
+            } else {
+                Node successor = min(node.right);
+                node.key = successor.key;
+                node.val = successor.val;
+                node.right = deleteMin(node.right);
+            }
+        }
+
+        node.size = 1 + size(node.left) + size(node.right);
+        return node;
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return min(node.left);
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null) {
+            return node.right;
+        }
+        node.left = deleteMin(node.left);
+        return node;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.size;
+    }
+
+    public Iterator<KeyValuePair> iterator() {
+        Queue<KeyValuePair> keyValuePairs = new LinkedList<>();
+        inorder(root, keyValuePairs);
+        return keyValuePairs.iterator();
+    }
+
+    private void inorder(Node node, Queue<KeyValuePair> keyValuePairs) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, keyValuePairs);
+        keyValuePairs.add(new KeyValuePair(node.key, node.val));
+        inorder(node.right, keyValuePairs);
+    }
 }
+
--- Main.java
+++ Main.java
@@ -1,2 +1,37 @@
+import java.util.Iterator;
 public class Main {
+    public static void main(String[] args) {
+        BST<Integer, String> bst = new BST<>();
+
+        // Insert key-value pairs into the binary search tree
+        bst.put(5, "Apple");
+        bst.put(3, "Banana");
+        bst.put(7, "Orange");
+        bst.put(2, "Mango");
+        bst.put(4, "Pineapple");
+        bst.put(6, "Grapes");
+        bst.put(8, "Kiwi");
+
+        // Print the size of the binary search tree
+        System.out.println("Size: " + bst.size());
+
+        // Get values by keys
+        System.out.println("Value for key 4: " + bst.get(4));
+        System.out.println("Value for key 7: " + bst.get(7));
+
+        // Delete a key from the binary search tree
+        bst.delete(3);
+
+        // Print the size after deletion
+        System.out.println("Size after deletion: " + bst.size());
+
+        // Iterate over the binary search tree using the iterator
+        System.out.println("Iterating over the binary search tree:");
+        Iterator<BST<Integer, String>.KeyValuePair> iterator = bst.iterator();
+        while (iterator.hasNext()) {
+            BST.KeyValuePair pair = iterator.next();
+            System.out.println("Key: " + pair.getKey() + ", Value: " + pair.getValue());
+        }
+    }
 }
+
